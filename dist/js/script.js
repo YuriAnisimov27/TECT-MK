42eebfed7da7ea54b40b94f0c0005cb23dbf8f93
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-11-06 14:09:30 +0300

    feat: implement slider after Directions block

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,7 @@
 import slider from './slider.js';
+import carousel from './carousel.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   slider();
+  carousel();
 });
--- dist/js/slider.js
+++ dist/js/slider.js
@@ -1,6 +1,11 @@
 function slider() {
   const sliderData = [
     {
+      h1: 'Взыскание дебиторской задолженности',
+      p: 'Индивидуальный подход и нацеленность на достижение результата',
+      background: 'url("../img/header-slider/Rectangle5.png") center'
+    },
+    {
       h1: 'Юридический аутсорсинг',
       p: 'Гарантия своевременности и качества услуг',
       background: 'url("../img/header-slider/Rectangle1.png") center'
@@ -19,12 +24,7 @@ function slider() {
       h1: 'Регистрация ООО',
       p: 'Гарантируем минимальные сроки регистрации под ключ ',
       background: 'url("../img/header-slider/Rectangle4.png") center'
-    },
-    {
-      h1: 'Взыскание дебиторской задолженности',
-      p: 'Индивидуальный подход и нацеленность на достижение результата',
-      background: 'url("../img/header-slider/Rectangle5.png") center'
-    },
+    }
   ];
   
   const arrowLeft = document.querySelector('.slider__nav-left');
@@ -32,19 +32,23 @@ function slider() {
   const slider = document.querySelector('.slider');
   const title = slider.querySelector('h1');
   const text = slider.querySelector('p');
-  let counter = 1;
+  let counter = 0;
+
+  function updateSliderData(index) {
+    slider.style.background = sliderData[index].background;
+    title.textContent = sliderData[index].h1;
+    text.textContent = sliderData[index].p;
+  }
 
   arrowLeft.addEventListener('click', () => {
-    slider.style.background = sliderData[Math.abs(counter++ % 5)].background;
-    title.textContent = sliderData[Math.abs(counter++ % 5)].h1;
-    text.textContent = sliderData[Math.abs(counter++ % 5)].p;
+    counter = (counter) ? counter - 1 : 4;
+    updateSliderData(counter);
     slider.style.transition = '0.6s ease';
   });
   
   arrowRight.addEventListener('click', () => {
-    slider.style.background = sliderData[Math.abs(counter-- % 5)].background;
-    title.textContent = sliderData[Math.abs(counter-- % 5)].h1;
-    text.textContent = sliderData[Math.abs(counter-- % 5)].p;
+    counter = Math.abs(counter + 1) % 5;
+    updateSliderData(counter);
     slider.style.transition = '0.6s ease';
   });
 }
